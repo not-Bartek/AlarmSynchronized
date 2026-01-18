22567663873030167cacc4680a856fc9c9cdb22b
Bartosz Dźwigała <b.dzwigala05@gmail.com>
2026-01-18 22:07:38 +0100

    final

--- main.c
+++ main.c
@@ -47,7 +47,52 @@ void *thread_func(void *arg)
     return NULL;
 }
 
-
+void do_work()
+{
+    int32_t time;
+    pthread_t thread;
+    char input[MAX_INPUT];
+    struct arguments *args;
+    sem_t semaphore;
+    if (sem_init(&semaphore, 0, FS_NUM) != 0)
+        ERR("sem_init");
+    while (work)
+    {
+        puts("Please enter the number of seconds for the alarm delay:");
+        if(fgets(input, MAX_INPUT, stdin) == NULL) {
+            if (errno == EINTR)
+                continue;
+            ERR("fgets:");
+        }
+        
+        time = atoi(input);
+        if(time <= 0) {
+            fputs("Incorrect time specified", stderr);
+            continue;
+        }
+        
+        if (TEMP_FAILURE_RETRY(sem_trywait(&semaphore)) == -1)
+        {
+            switch (errno)
+            {
+                case EAGAIN:
+                    fprintf(stderr, "Only %d alarms can be set at the time.", FS_NUM);
+                case EINTR:
+                    continue;
+            }
+            ERR("sem_trywait");
+        }
+        
+        if ((args = (struct arguments *)malloc(sizeof(struct arguments))) == NULL)
+            ERR("malloc:");
+        args->time = time;
+        args->semaphore = &semaphore;
+        if (pthread_create(&thread, NULL, thread_func, (void *) args) != 0)
+            ERR("pthread_create");
+        if (pthread_detach(thread) != 0)
+            ERR("pthread_detach");
+    }
+}
 
 int main()
 {
